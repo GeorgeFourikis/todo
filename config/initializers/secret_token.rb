c89a4c35c0c6dc9e761b96b09bbc80ef9e1caed7
GeorgeFourikis <you@example.cogit config --global user.email you@example.co>
2016-06-20 17:36:59 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todo::Application.config.secret_key_base = '769537bf7058485a2c3c322032a8f71c0de677576ac39409bbdd6a42d6dfe8c4f6ca258d3b0e35c04b81e48bfcc508f7376637c4b417463769e89fc7e9fa8d0f'
